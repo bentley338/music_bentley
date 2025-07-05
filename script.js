df1e47f2d48f7c7b81edec9b27fe139a7c70addb
bentley338 <graha9181@gmail.com>
2025-07-05 17:29:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- BASE URL UNTUK FILE MEDIA (SANGAT PENTING!) ---
     // GANTI INI DENGAN URL DASAR GITHUB PAGES ANDA!
     // Contoh: 'https://yourusername.github.io/your-repository-name/'
-    const BASE_URL = 'https://bentley16.github.io/MelodyVerse/'; //
+    const BASE_URL = 'https://github.com/bentley338/music_bentley'; //
 
     // --- DATA LAGU (LOAD DARI LOCALSTORAGE ATAU DEFAULT) ---
     // Lagu-lagu default akan menyimpan HANYA nama file.
